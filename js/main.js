0d014e5e395fd50bf26f174ff517bad748fab7dd
RomaVintonyak <19roman23@gmail.com>
2021-02-28 12:17:21 +0200

    create rotate money script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-jQuery(document).ready(function(){
-    "use script";
-  
+jQuery(document).ready(function () {
+  "use script";
+  //transform scroll script
+  var moneyTop = $(".money__wraper").offset().top;
+  var moneyHeight = $(".money__wraper").height();
+  $(document).on("scroll", function () {
+    if ($(this).scrollTop() >= moneyTop - moneyHeight) {
+      $(".money__wraper").addClass("money__wraper--active");
+    } else {
+      $(".money__wraper").removeClass("money__wraper--active");
+    }
+  });
 });
